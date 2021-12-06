8425cd5177e9c8b74e98f7f3c3b96f0fb3840952
Nay Hanif <71371234+NayHanif@users.noreply.github.com>
2021-12-06 15:55:19 +0700

    Update Filters.php

--- Pertemuan 14/aplikasi-pegawai/app/Config/Filters.php
+++ Pertemuan 14/aplikasi-pegawai/app/Config/Filters.php
@@ -19,7 +19,7 @@ class Filters extends BaseConfig
         'csrf' => \CodeIgniter\Filters\CSRF::class,
         'toolbar' => \CodeIgniter\Filters\DebugToolbar::class,
         'honeypot' => \CodeIgniter\Filters\Honeypot::class,
-        'authfilter' => \App\Filters\AuthFilter::class
+        'authfilter' => \App\Filters\Auth::class
     ];
         
 
